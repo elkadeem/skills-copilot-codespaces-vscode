88681b252deb0a4d733070998fdc4b337e9ebc5e
Wael Elkadim <elkadeem@hotmail.com>
2024-01-24 05:50:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,14 @@
+// Create Web Server
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+    res.end('Hello, World!');
 });
 
 server.listen(3000, 'localhost', () => {
-    console.log('Server is running at http://localhost:3000/');
+    console.log('Server running at http://localhost:3000/');
 });
 
